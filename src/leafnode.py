708c21038becbfc1cfeaf69320f56e0bf81d55b9
bry-val <brvlncia@gmail.com>
2024-07-01 23:17:22 -0500

    Additional tests added for LeadNode

--- src/leafnode.py
+++ src/leafnode.py
@@ -2,8 +2,8 @@ from htmlnode import HTMLNode
 
 
 class LeafNode(HTMLNode):
-    def __init__(self, tag, value, props):
-        super().__init__(tag, value, props)
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag=tag, value=value, props=props)
 
     def to_html(self):
         if self.value is None:
@@ -15,5 +15,4 @@ class LeafNode(HTMLNode):
     def string_builder(self):
         if self.props is None:
             return f"<{self.tag}>{self.value}</{self.tag}>"
-        else:
-            return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
+        return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -7,8 +7,13 @@ class TestLeafNode(unittest.TestCase):
     def test_to_html(self):
         node1 = LeafNode("p", "This is a paragraph of text.")
         node2 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        node3 = LeafNode("p", "This is nothing",
+                         {"href": "https://www.google.com", "target": "_blank"})
+        with self.assertRaises(TypeError):
+            LeafNode('a', 'b', 'c', 'd')
         self.assertEqual(node1.to_html(), '<p>This is a paragraph of text.</p>')
-        self.assertEqual(node2, '<a href="https://www.google.com">Click me!</a>')
+        self.assertEqual(node2.to_html(), '<a href="https://www.google.com">Click me!</a>')
+        self.assertEqual(node3.to_html(), '<p href="https://www.google.com" target="_blank">This is nothing</p>')
 
 
 if __name__ == "__main__":
